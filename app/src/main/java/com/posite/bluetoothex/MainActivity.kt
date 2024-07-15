d5e4be7ab629f3c825f257379da4790a1cda134b
posite <su84053@gmail.com>
2024-07-15 23:45:33 +0900

    feat: Bluetooth 구현에 필요한 Permission 요구

--- app/src/main/java/com/posite/bluetoothex/MainActivity.kt
+++ app/src/main/java/com/posite/bluetoothex/MainActivity.kt
@@ -1,47 +1,106 @@
 package com.posite.bluetoothex
 
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.Manifest.permission.BLUETOOTH
+import android.Manifest.permission.BLUETOOTH_ADMIN
+import android.Manifest.permission.BLUETOOTH_CONNECT
+import android.Manifest.permission.BLUETOOTH_SCAN
+import android.bluetooth.BluetoothManager
+import android.bluetooth.le.BluetoothLeScanner
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.posite.bluetoothex.ui.theme.BluetoothExTheme
 
 class MainActivity : ComponentActivity() {
+    private val bluetooth by lazy {
+        this@MainActivity.getSystemService(Context.BLUETOOTH_SERVICE)
+                as? BluetoothManager
+            ?: throw Exception("Bluetooth is not supported by this device")
+    }
+
+    private val scanner: BluetoothLeScanner
+        get() = bluetooth.adapter.bluetoothLeScanner
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             BluetoothExTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
+                    ) {
+                        if (!haveAllPermissions(this@MainActivity)) {
+                            GrantPermissionsButton {
+                                Toast.makeText(
+                                    this@MainActivity,
+                                    "Bluetooth Permissions granted",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                        } else {
+
+                        }
+
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+
+private val ALL_BLE_PERMISSIONS = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+    arrayOf(
+        BLUETOOTH_CONNECT,
+        BLUETOOTH_SCAN
+    )
+} else {
+    arrayOf(
+        BLUETOOTH_ADMIN,
+        BLUETOOTH,
+        ACCESS_FINE_LOCATION
     )
 }
 
-@Preview(showBackground = true)
+private fun haveAllPermissions(context: Context) =
+    ALL_BLE_PERMISSIONS
+        .all { context.checkSelfPermission(it) == PackageManager.PERMISSION_GRANTED }
+
 @Composable
-fun GreetingPreview() {
-    BluetoothExTheme {
-        Greeting("Android")
+fun GrantPermissionsButton(onPermissionGranted: () -> Unit) {
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestMultiplePermissions()
+    ) { granted ->
+        if (granted.values.all { it }) {
+            // User has granted all permissions
+            onPermissionGranted()
+        } else {
+            // TODO: handle potential rejection in the usual way
+        }
+    }
+
+    // User presses this button to request permissions
+    Button(
+        onClick = { launcher.launch(ALL_BLE_PERMISSIONS) }) {
+        Text("Grant Permission")
     }
 }
